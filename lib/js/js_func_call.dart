b73916e76322eb08de13f0931badab86ea649312
morichan1 <105962626+morichan1@users.noreply.github.com>
2023-10-23 22:26:03 +0900

    、

---
+++ lib/js/js_func_call.dart
@@ -0,0 +1,22 @@
+@JS()
+// jsファイル名を記載
+library js_func;
+import 'package:js/js.dart';
+
+// jsファイルから呼び出したい関数をCall
+@JS('callJsFunc')
+external String callJsFunc(String message);
+
+//音声入力を開始するもの
+@JS('vr_function')
+external void vr_function();
+
+//音声入力したものをflutter側におくる
+@JS('createList')
+external List createList();
+
+//文章を切る。listを削除する
+@JS('endRecoding')
+external void endRecoding();
+
+
